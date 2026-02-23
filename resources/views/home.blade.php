150fe78a1fc175bc6985f9a95ce15376816b7c68
PreciousNwogu <nwoguprecious93@gmail.com>
2026-02-23 16:40:01 -0500

    fix: forward route variables to home.remote via $__data so CMS services render

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 {{-- Wrapper view for compatibility: render the existing file `home.remote.blade.php` by absolute path.
 	This avoids Blade's view-name resolution which treats dots as directory separators.
+	$__data forwards all route-passed variables (servicePrices, extraServices, etc.) into the sub-view.
 --}}
-{!! view()->file(resource_path('views/home.remote.blade.php'))->render() !!}
+{!! view()->file(resource_path('views/home.remote.blade.php'), $__data)->render() !!}
